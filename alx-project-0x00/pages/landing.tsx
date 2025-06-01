7df24b3afae00c57c873967552a5d4f7ed3a2937
Meron Solomon <meronsolomondana@gmail.com>
2025-06-01 12:34:33 +0300

    update button

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -10,10 +10,30 @@ const Landing: React.FC = () => {
 
       <div className="p-6 space-y-4">
         <h1 className="text-2xl font-semibold">Landing Page</h1>
-        <Button title="Small Button" size="small" shape="rounded-sm" />
-        <Button title="Medium Button" size="medium" shape="rounded-md" />
-        <Button title="Extra Button" size="large" shape="rounded-lg" />
-        <Button title="Full Button" size="medium" shape="rounded-full"/>
+        <Button
+          title="Small Button"
+          size="small"
+          shape="rounded-sm"
+          styles="bg-blue-500 text-white"
+        />
+        <Button
+          title="Medium Button"
+          size="medium"
+          shape="rounded-md"
+          styles="bg-green-500 text-white"
+        />
+        <Button
+          title="Large Button"
+          size="large"
+          shape="rounded-full"
+          styles="bg-purple-500 text-white"
+        />
+        <Button
+          title="Full Button"
+          size="medium"
+          shape="rounded-full"
+          styles="bg-red-500 text-white"
+        />
       </div>
     </div>
   );
